c3178e31c9d69ad4a8112fc50bf606f15c1a856e
Sami Ali <samibaloch13@hotmail.com>
2022-06-10 20:20:58 -0700

    commiting variable exercie

--- app.js
+++ app.js
@@ -4,7 +4,39 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Sami"
+let lastName = "string";
+var age;
 
 // Exercise 2
+firstName;
+lastName = "Ali";
+age = 26
+
+console.log(firstName, lastName, age);
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language, createdYear, isCaseSensitive);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize);
+
+let title = "Name os the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+console.log(title, author, pageCount, bookMark, hasRead);
+
+
